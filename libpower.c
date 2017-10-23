83e7347e7761cbabe510a1adfa746f3b802b29bf
nutgut <tommy.hecken@gmail.com>
2017-10-23 20:35:59 +0200

    Läser in header

--- libpower.c
+++ libpower.c
@@ -1,9 +1,5 @@
 #include <stdio.h>
-<<<<<<< HEAD
 #include "power.h"
-=======
-// #include "power.h"
->>>>>>> 8bc9b4ce4baa9d3c0c70c5992f8e89c7944903e8
 
 //Calculates totally power process in a circuit (in 2 ways)
 float calc_power_r(float volt, float resistance)
